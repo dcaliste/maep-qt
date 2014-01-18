b6d69c6675a28311ca48ea7cb64afda73cde6328
Damien Caliste <damien.caliste@cea.fr>
2014-01-18 14:57:57 +0100

    Use a correct user agent for tile downloading.

--- src/osm-gps-map/osm-gps-map.c
+++ src/osm-gps-map/osm-gps-map.c
@@ -43,7 +43,8 @@
 #include "osm-gps-map.h"
 
 #define ENABLE_DEBUG                (0)
-#define USER_AGENT                  "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11"
+/* #define USER_AGENT                  "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11" */
+#define USER_AGENT                  PACKAGE "-libsoup/" VERSION
 
 struct _OsmGpsMapPrivate
 {
