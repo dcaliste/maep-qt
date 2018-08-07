35f3e2bb2873c41314133260f1ba1c3e99ce3b6c
Damien Caliste <dcaliste@free.fr>
2018-08-07 11:41:53 +0200

    Add Spanish translation, contributed by Carmen Fernández B.

---
+++ translations/es.ts
@@ -0,0 +1,432 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>Miscellaneous</source>
+        <translation>Varios</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licencia</translation>
+    </message>
+    <message>
+        <source>Authors</source>
+        <translation>Autores</translation>
+    </message>
+    <message>
+        <source>About Mæp</source>
+        <translation>Acerca de Mæp</translation>
+    </message>
+    <message>
+        <source>A small and fast tile map</source>
+        <translation>Un mapa de teselas pequeño y rápido</translation>
+    </message>
+    <message>
+        <source>Report bugs</source>
+        <translation>Informar de errores</translation>
+    </message>
+    <message>
+        <source>Please report bugs or feature requests via the Mæp bug tracker. This bug tracker can directly be reached via the following link:</source>
+        <translation>Informa de errores o solicita funcionalidades a través de la herramienta de seguimiento de errores, cuyo enlace es el siguiente:</translation>
+    </message>
+    <message>
+        <source>Open bug tracker</source>
+        <translation>Abrir seguimiento de errores</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Donar</translation>
+    </message>
+    <message>
+        <source>If you like Mæp and want to support its future development please consider donating to the developer. You can either donate via paypal to till@harbaum.org or you can just click the button below which will open the appropriate web page in your browser.</source>
+        <translation>Si te gusta Mæp y quieres apoyar su futuro desarrollo, puedes hacer una donación al desarrollador. Puedes donar a través de Paypal a till@harbaum.org o haciendo clic en el botón de abajo, que abrirá la página Web correspondiente en tu navegador.</translation>
+    </message>
+    <message>
+        <source>Donate through Paypal</source>
+        <translation>Donar por Paypal</translation>
+    </message>
+    <message>
+        <source>Version %1 - %2</source>
+        <translation>Versión %1 - %2</translation>
+    </message>
+    <message>
+        <source>Copyright %1</source>
+        <translation>Derechos de autor %1</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <source>enter a new file name</source>
+        <translation>indica nuevo nombre</translation>
+    </message>
+    <message>
+        <source>or select an existing one</source>
+        <translation>o selecciona archivo existente</translation>
+    </message>
+    <message>
+        <source>My Jolla</source>
+        <translation>Carpeta personal</translation>
+    </message>
+    <message>
+        <source>No files</source>
+        <translation>No hay archivos</translation>
+    </message>
+    <message>
+        <source>sort by</source>
+        <translation>orden por</translation>
+    </message>
+    <message>
+        <source>name</source>
+        <translation>nombre</translation>
+    </message>
+    <message>
+        <source>date</source>
+        <translation>fecha</translation>
+    </message>
+    <message>
+        <source>type</source>
+        <translation>tipo</translation>
+    </message>
+    <message>
+        <source>reversed</source>
+        <translation>inverso</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceHeader</name>
+    <message>
+        <source>Enter a place name</source>
+        <translation>Introduce nombre de lugar</translation>
+    </message>
+    <message>
+        <source>Searching…</source>
+        <translation>Buscando...</translation>
+    </message>
+    <message>
+        <source>Place search</source>
+        <translation>Búsqueda de lugar</translation>
+    </message>
+    <message>
+        <source>search</source>
+        <translation>buscar</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n place(s) found</source>
+        <translation><numerusform>%n lugar encontrado</numerusform><numerusform>%n lugares encontrados</numerusform></translation>
+    </message>
+    <message>
+        <source>at %L1 km</source>
+        <translation>a %L1 km</translation>
+    </message>
+    <message>
+        <source>at %1 m</source>
+        <translation>a %1 m</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Draw map double pixel</source>
+        <translation>Dibujar pixeles a tamaño doble</translation>
+    </message>
+    <message>
+        <source>The tiles of the zoom level above are used instead of the normal ones. The rendering appears as if zoomed.</source>
+        <translation>Se usan teselas a un nivel de zoom superior al normal. El mapa se mostrará como si el zoom estuviera ampliado.</translation>
+    </message>
+    <message>
+        <source>Compass mode</source>
+        <translation>Modo brújula</translation>
+    </message>
+    <message>
+        <source>Either point towards physical north pole, or show device orientation relative to map. When switched off, the sensor is completely disabled.</source>
+        <translation>Señala el polo norte físico o muestra la orientacion del dispositivo respecto del mapa. Cuando esta opción está desactivada, el sensor está completamente desactivado.</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Apagado</translation>
+    </message>
+    <message>
+        <source>Point north</source>
+        <translation>Polo norte</translation>
+    </message>
+    <message>
+        <source>Device orientation</source>
+        <translation>Orientación dispositivo</translation>
+    </message>
+    <message>
+        <source>GPS refresh rate</source>
+        <translation>Actualización del GPS</translation>
+    </message>
+    <message>
+        <source>no update</source>
+        <translation>sin actualizar</translation>
+    </message>
+    <message>
+        <source>Minimum time between two GPS updates. If set to 0, there will be no GPS update.</source>
+        <translation>Tiempo mínimo entre dos actualizaciones de GPS. Si se configura a 0, el GPS no se actualizará.</translation>
+    </message>
+    <message>
+        <source>Track autosave rate</source>
+        <translation>Auto-guardado de trayectos</translation>
+    </message>
+    <message>
+        <source>on demand</source>
+        <translation>a demanda</translation>
+    </message>
+    <message>
+        <source>Time between two automatic track dumps on disk. Autosaving a track without providing a filename will result to the track being saved in $HOME/.local/share/harbour-maep-qt/track.gpx</source>
+        <translation>Tiempo entre dos grabaciones del trayecto en disco. El guardado automático de un trayecto sin indicar un nombre de archivo dará como resultado que el trayecto se guarde en $HOME/.local/share/harbour-maep-qt/track.gpx</translation>
+    </message>
+    <message>
+        <source>Track metric accuracy</source>
+        <translation>Precisión en metros del trayecto</translation>
+    </message>
+    <message>
+        <source>keep all</source>
+        <translation>mantener todos</translation>
+    </message>
+    <message>
+        <source>Points in track are displayed if horizontal accuracy is below this value.</source>
+        <translation>Se muestran los puntos del trayecto cuya precisión horizontal es inferior a este valor.</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <translation>%1 s</translation>
+    </message>
+    <message>
+        <source>%1 min.</source>
+        <translation>%1 min.</translation>
+    </message>
+    <message>
+        <source>%1 m</source>
+        <translation>%1 m</translation>
+    </message>
+</context>
+<context>
+    <name>Sources</name>
+    <message>
+        <source>Manage tile sources</source>
+        <translation>Gestión de fuentes de teselas</translation>
+    </message>
+    <message>
+        <source>Select a tile source</source>
+        <translation>Seleccionar una fuente de tesela</translation>
+    </message>
+    <message>
+        <source>Long tap to display options</source>
+        <translation>Mantén pulsado para mostrar opciones</translation>
+    </message>
+    <message>
+        <source>Switch to selection</source>
+        <translation>Cambiar a modo selección</translation>
+    </message>
+    <message>
+        <source>Switch to management</source>
+        <translation>Cambiar a modo gestión</translation>
+    </message>
+    <message>
+        <source>base tiles</source>
+        <translation>mapas base</translation>
+    </message>
+    <message>
+        <source>overlay tiles</source>
+        <translation>capas</translation>
+    </message>
+    <message>
+        <source>Open map copyright in browser</source>
+        <translation>Abrir derechos de autor en navegador</translation>
+    </message>
+</context>
+<context>
+    <name>TrackHeader</name>
+    <message>
+        <source>Clear current track</source>
+        <translation>Limpiar trayecto actual</translation>
+    </message>
+    <message>
+        <source>Track</source>
+        <translation>Trayecto</translation>
+    </message>
+    <message>
+        <source>No track</source>
+        <translation>No hay trayectos</translation>
+    </message>
+    <message>
+        <source>capture GPS position</source>
+        <translation>capturar posición GPS</translation>
+    </message>
+    <message>
+        <source>import a local file</source>
+        <translation>importar archivo local</translation>
+    </message>
+    <message>
+        <source>last autosaved track</source>
+        <translation>último trayecto auto-guardado</translation>
+    </message>
+</context>
+<context>
+    <name>TrackView</name>
+    <message>
+        <source>track width</source>
+        <translation>grosor del trayecto</translation>
+    </message>
+    <message>
+        <source>clear</source>
+        <translation>limpiar</translation>
+    </message>
+    <message>
+        <source>save on device</source>
+        <translation>guardar en dispositivo</translation>
+    </message>
+    <message>
+        <source>Unsaved track</source>
+        <translation>trayecto no guardado</translation>
+    </message>
+    <message>
+        <source>no accurate data</source>
+        <translation>datos no precisos</translation>
+    </message>
+    <message>
+        <source>new waypoint description</source>
+        <translation>descripción de nuevo punto de ruta</translation>
+    </message>
+    <message>
+        <source>new waypoint at GPS position</source>
+        <translation>posición GPS de nuevo punto de ruta</translation>
+    </message>
+    <message>
+        <source>add</source>
+        <translation>añadir</translation>
+    </message>
+    <message>
+        <source>cancel</source>
+        <translation>cancelar</translation>
+    </message>
+    <message>
+        <source>update</source>
+        <translation>actualizar</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <translation>%1 s</translation>
+    </message>
+    <message>
+        <source>%1 min</source>
+        <translation>%1 min</translation>
+    </message>
+    <message>
+        <source>%1 h %2</source>
+        <translation>%1 h %2</translation>
+    </message>
+    <message>
+        <source>%L1 km</source>
+        <translation>%L1 km</translation>
+    </message>
+    <message>
+        <source>%1 m</source>
+        <translation>%1 m</translation>
+    </message>
+    <message>
+        <source>%1 km/h</source>
+        <translation>%1 km/h</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>About Mæp</source>
+        <translation>Acerca de Mæp</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Screen rotation</source>
+        <translation>Rotación de pantalla</translation>
+    </message>
+    <message>
+        <source>Wikipedia</source>
+        <translation>Wikipedia</translation>
+    </message>
+    <message>
+        <source>Track capture</source>
+        <translation>Capturar trayecto</translation>
+    </message>
+    <message>
+        <source>import track from device</source>
+        <translation>importar trayecto desde dispositivo</translation>
+    </message>
+    <message>
+        <source>no accurate data</source>
+        <translation>datos no precisos</translation>
+    </message>
+    <message>
+        <source>Open Wikipedia page</source>
+        <translation>Abrir página Wikipedia</translation>
+    </message>
+    <message>
+        <source>Save current track</source>
+        <translation>Guardar trayecto actual</translation>
+    </message>
+    <message>
+        <source>Select a track file</source>
+        <translation>Seleccionar trayecto</translation>
+    </message>
+    <message>
+        <source>Loading a track file</source>
+        <translation>Cargando trayecto</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <translation>%1 s</translation>
+    </message>
+    <message>
+        <source>%1 min</source>
+        <translation>%1 min</translation>
+    </message>
+    <message>
+        <source>%1 h %2</source>
+        <translation>%1 h %2</translation>
+    </message>
+    <message>
+        <source>%L1 km</source>
+        <translation>%L1 km</translation>
+    </message>
+    <message>
+        <source>%1 m</source>
+        <translation>%1 m</translation>
+    </message>
+    <message>
+        <source>%1 km/h</source>
+        <translation>%1 km/h</translation>
+    </message>
+    <message>
+        <source>at %L1 km</source>
+        <translation>a %L1 km</translation>
+    </message>
+    <message>
+        <source>at %1 m</source>
+        <translation>a %1 m</translation>
+    </message>
+    <message>
+        <source>coordinates: %1
+</source>
+        <translation>coordenadas: %1
+</translation>
+    </message>
+    <message>
+        <source>source: geonames.org</source>
+        <translation>fuente: geonames.org</translation>
+    </message>
+    <message>
+        <source>Overwrite file?</source>
+        <translation>¿Sobrescribir archivo?</translation>
+    </message>
+</context>
+</TS>
