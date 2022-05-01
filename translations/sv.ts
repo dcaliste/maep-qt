b72617c85d16db7fb4b98a4351f77f939c754c25
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-05-01 08:38:19 +0200

    Create sv.ts
    Add Swedish translation, please.

---
+++ translations/sv.ts
@@ -0,0 +1,437 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <source>Miscellaneous</source>
+        <translation>Diverse</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message>
+        <source>Authors</source>
+        <translation>Utvecklare</translation>
+    </message>
+    <message>
+        <source>About Mæp</source>
+        <translation>Om Maep</translation>
+    </message>
+    <message>
+        <source>A small and fast tile map</source>
+        <translation>En liten och snabb kartapp</translation>
+    </message>
+    <message>
+        <source>Report bugs</source>
+        <translation>Rapportera fel</translation>
+    </message>
+    <message>
+        <source>Please report bugs or feature requests via the Mæp bug tracker. This bug tracker can directly be reached via the following link:</source>
+        <translation>Vänligen rapportera fel och funktionsförfrågningar via Maep ärendehanterare. Ärendehanteraren kan nås direkt via följande länk:</translation>
+    </message>
+    <message>
+        <source>Open bug tracker</source>
+        <translation>Öppna ärendehanteraren</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Donera</translation>
+    </message>
+    <message>
+        <source>If you like Mæp and want to support its future development please consider donating to the developer. You can either donate via paypal to till@harbaum.org or you can just click the button below which will open the appropriate web page in your browser.</source>
+        <translation>Vverväg att donera till utvecklaren, om du gillar Mæp och vill stödja dess framtida utveckling. Du kan antingen donera via PayPal till till@harbaum.org eller så kan du bara klicka på knappen nedan som öppnar lämplig webbsida i din webbläsare.</translation>
+    </message>
+    <message>
+        <source>Donate through Paypal</source>
+        <translation>Donera via PayPal</translation>
+    </message>
+    <message>
+        <source>Version %1 - %2</source>
+        <translation>Version %1 - %2</translation>
+    </message>
+    <message>
+        <source>Copyright %1</source>
+        <translation>Copyright %1</translation>
+    </message>
+</context>
+<context>
+    <name>FileChooser</name>
+    <message>
+        <source>enter a new file name</source>
+        <translation>Ange ett nytt filnamn</translation>
+    </message>
+    <message>
+        <source>or select an existing one</source>
+        <translation>eller välj ett befintligt</translation>
+    </message>
+    <message>
+        <source>My Jolla</source>
+        <translation>Min Jolla</translation>
+    </message>
+    <message>
+        <source>No files</source>
+        <translation>Inga filer</translation>
+    </message>
+    <message>
+        <source>sort by</source>
+        <translation>Sortera efter</translation>
+    </message>
+    <message>
+        <source>name</source>
+        <translation>namn</translation>
+    </message>
+    <message>
+        <source>date</source>
+        <translation>datum</translation>
+    </message>
+    <message>
+        <source>type</source>
+        <translation>typ</translation>
+    </message>
+    <message>
+        <source>reversed</source>
+        <translation>omvänt</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceHeader</name>
+    <message>
+        <source>Enter a place name</source>
+        <translation>Ange ett platsnamn</translation>
+    </message>
+    <message>
+        <source>Searching…</source>
+        <translation>Söker…</translation>
+    </message>
+    <message>
+        <source>Place search</source>
+        <translation>Platssök</translation>
+    </message>
+    <message>
+        <source>search</source>
+        <translation>Sök</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n place(s) found</source>
+        <translation>
+            <numerusform>%n plats hittad</numerusform>
+            <numerusform>%n platser hittade</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>at %L1 km</source>
+        <translation>på %L1 km</translation>
+    </message>
+    <message>
+        <source>at %1 m</source>
+        <translation>på %1 m</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Draw map double pixel</source>
+        <translation>Rita upp kart-dubbelpixel</translation>
+    </message>
+    <message>
+        <source>The tiles of the zoom level above are used instead of the normal ones. The rendering appears as if zoomed.</source>
+        <translation>Kartrutorna på zoomnivån ovan används i stället för de normala. Renderingen ser ut som om den är zoomad.</translation>
+    </message>
+    <message>
+        <source>Compass mode</source>
+        <translation>Kompassläge</translation>
+    </message>
+    <message>
+        <source>Either point towards physical north pole, or show device orientation relative to map. When switched off, the sensor is completely disabled.</source>
+        <translation>Pekar antingen mot fysisk nordpol eller visar enhetens orientering i förhållande till kartan. När sensorn är avstängd är den helt inaktiverad.</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Av</translation>
+    </message>
+    <message>
+        <source>Point north</source>
+        <translation>Peka mot norr</translation>
+    </message>
+    <message>
+        <source>Device orientation</source>
+        <translation>Enhetsorientering</translation>
+    </message>
+    <message>
+        <source>GPS refresh rate</source>
+        <translation>GPS-uppdateringsfrekvens</translation>
+    </message>
+    <message>
+        <source>no update</source>
+        <translation>ingen uppdatering</translation>
+    </message>
+    <message>
+        <source>Minimum time between two GPS updates. If set to 0, there will be no GPS update.</source>
+        <translation>Minsta tid mellan två GPS-updateringar. Vid 0, blir det ingen GPS-uppdatering.</translation>
+    </message>
+    <message>
+        <source>Track autosave rate</source>
+        <translation>Automatisk spårdumpfrekvens</translation>
+    </message>
+    <message>
+        <source>on demand</source>
+        <translation>på begäran</translation>
+    </message>
+    <message>
+        <source>Time between two automatic track dumps on disk. Autosaving a track without providing a filename will result to the track being saved in $HOME/.local/share/harbour-maep-qt/track.gpx</source>
+        <translation>Tid mellan två automatiska spårdumpar på disken. Om du automatiskt sparar ett spår utan att ange filnamn sparas spåret i $HOME/.local/share/harbour-maep-qt/track.gpx</translation>
+    </message>
+    <message>
+        <source>Track metric accuracy</source>
+        <translation>Metrisk spårnogrannhet</translation>
+    </message>
+    <message>
+        <source>keep all</source>
+        <translation>behåll alla</translation>
+    </message>
+    <message>
+        <source>Points in track are displayed if horizontal accuracy is below this value.</source>
+        <translation>Punkter i spåret visas om vågrät noggrannhet är under detta värde.</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <translation>%1 s</translation>
+    </message>
+    <message>
+        <source>%1 min.</source>
+        <translation>%1 min.</translation>
+    </message>
+    <message>
+        <source>%1 m</source>
+        <translation>%1 m</translation>
+    </message>
+</context>
+<context>
+    <name>Sources</name>
+    <message>
+        <source>Manage tile sources</source>
+        <translation>Hantera källor</translation>
+    </message>
+    <message>
+        <source>Select a tile source</source>
+        <translation>Välj kartkälla</translation>
+    </message>
+    <message>
+        <source>Long tap to display options</source>
+        <translation>Långtryck för att visa alternativ</translation>
+    </message>
+    <message>
+        <source>Switch to selection</source>
+        <translation>Växla till markerat</translation>
+    </message>
+    <message>
+        <source>Switch to management</source>
+        <translation>Växla till hantering</translation>
+    </message>
+    <message>
+        <source>base tiles</source>
+        <translation>Basrutor</translation>
+    </message>
+    <message>
+        <source>overlay tiles</source>
+        <translation>Överläggsrutor</translation>
+    </message>
+    <message>
+        <source>Open map copyright in browser</source>
+        <translation>Öppna kart-copyright i webbläsaren</translation>
+    </message>
+</context>
+<context>
+    <name>TrackHeader</name>
+    <message>
+        <source>Clear current track</source>
+        <translation>Rensa aktuellt spår</translation>
+    </message>
+    <message>
+        <source>Track</source>
+        <translation>Spår</translation>
+    </message>
+    <message>
+        <source>No track</source>
+        <translation>Inget spår</translation>
+    </message>
+    <message>
+        <source>capture GPS position</source>
+        <translation>Hämta GPS-position</translation>
+    </message>
+    <message>
+        <source>import a local file</source>
+        <translation>Importera en lokal fil</translation>
+    </message>
+    <message>
+        <source>last autosaved track</source>
+        <translation>Senast automatiskt sparade spår</translation>
+    </message>
+</context>
+<context>
+    <name>TrackView</name>
+    <message>
+        <source>track width</source>
+        <translation>Spårbredd</translation>
+    </message>
+    <message>
+        <source>clear</source>
+        <translation>Rensa</translation>
+    </message>
+    <message>
+        <source>save on device</source>
+        <translation>Spara på enheten</translation>
+    </message>
+    <message>
+        <source>Unsaved track</source>
+        <translation>Osparat spår</translation>
+    </message>
+    <message>
+        <source>no accurate data</source>
+        <translation>Ingen exakt data</translation>
+    </message>
+    <message>
+        <source>new waypoint description</source>
+        <translation>Ingen vägpunktsbeskrivning</translation>
+    </message>
+    <message>
+        <source>new waypoint at GPS position</source>
+        <translation>Ingen vägpunkt vid GPS-positionen</translation>
+    </message>
+    <message>
+        <source>add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>update</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <translation>%1 s</translation>
+    </message>
+    <message>
+        <source>%1 min</source>
+        <translation>%1 min</translation>
+    </message>
+    <message>
+        <source>%1 h %2</source>
+        <translation>%1 t %2</translation>
+    </message>
+    <message>
+        <source>%L1 km</source>
+        <translation>%L1 km</translation>
+    </message>
+    <message>
+        <source>%1 m</source>
+        <translation>%1 m</translation>
+    </message>
+    <message>
+        <source>%1 km/h</source>
+        <translation>%1 km/t</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>About Mæp</source>
+        <translation>Om Maep</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Screen rotation</source>
+        <translation>Skärmrotation</translation>
+    </message>
+    <message>
+        <source>Wikipedia</source>
+        <translation>Wikipedia</translation>
+    </message>
+    <message>
+        <source>Track capture</source>
+        <translation>Spårinspelning</translation>
+    </message>
+    <message>
+        <source>import track from device</source>
+        <translation>Importera spår från enheten</translation>
+    </message>
+    <message>
+        <source>no accurate data</source>
+        <translation>Ingen exakt data</translation>
+    </message>
+    <message>
+        <source>Open Wikipedia page</source>
+        <translation>Öppna Wikipedia-sidan</translation>
+    </message>
+    <message>
+        <source>Save current track</source>
+        <translation>Spara aktuellt spår</translation>
+    </message>
+    <message>
+        <source>Select a track file</source>
+        <translation>Välj en spårfil</translation>
+    </message>
+    <message>
+        <source>Loading a track file</source>
+        <translation>Läser in en spårfil</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <translation>%1 s</translation>
+    </message>
+    <message>
+        <source>%1 min</source>
+        <translation>%1 min</translation>
+    </message>
+    <message>
+        <source>%1 h %2</source>
+        <translation>%1 t %2</translation>
+    </message>
+    <message>
+        <source>%L1 km</source>
+        <translation>%L1 km</translation>
+    </message>
+    <message>
+        <source>%1 m</source>
+        <translation>%1 m</translation>
+    </message>
+    <message>
+        <source>%1 km/h</source>
+        <translation>%1 km/t</translation>
+    </message>
+    <message>
+        <source>at %L1 km</source>
+        <translation>%L1 km</translation>
+    </message>
+    <message>
+        <source>at %1 m</source>
+        <translation>%1 m</translation>
+    </message>
+    <message>
+        <source>coordinates: %1
+</source>
+        <translation>Coordinater: %1
+</translation>
+    </message>
+    <message>
+        <source>source: geonames.org</source>
+        <translation>Källa: geonames.org</translation>
+    </message>
+    <message>
+        <source>Overwrite file?</source>
+        <translation>Vill du skriva över filen?</translation>
+    </message>
+</context>
+</TS>
